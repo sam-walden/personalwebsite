59f7872b1f4a4b2a8e74b39a192e5cd24c8b6dab
Sam Walden <sam.alien@yahoo.com>
2020-04-09 01:12:13 +0100

    database added

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://personalwebsite:winter10@personalwebsite-v8bwc.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(keys.MongoURI)
+.then(()=>{
+    console.log('Remote MongoDB is connected...');
+});
 const app = express();
 
 app.engine('handlebars', exphbs({
@@ -8,6 +15,26 @@ app.engine('handlebars', exphbs({
 
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+
+//setup body parser to encode url
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+//create collection with mongoose
+const Schema = mongoose.Schema;
+const Message = mongoose.model('message',new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    message: {
+        type: String
+    }
+}))
 
 const port = process.env.PORT || 3000;
 
@@ -22,10 +49,21 @@ app.get('/about', (req,res) =>{
 app.get('/contact', (req,res) => {
     res.render('contact');
 });
+app.post('/getMessage',(req,res) =>{
+    const newMessage= {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    }
+    new Message(newMessage).save()
+    .then(()=>{
+        res.render('inbox');
+    });
+});
 // this is porfolio route handler
 app.get('/portfolio', (req,res) => {
     res.render('portfolio');
-})
+});
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
